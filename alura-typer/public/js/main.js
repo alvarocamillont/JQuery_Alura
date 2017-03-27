8255815636f26fceb0318e7d7436d942e4e0dd3c
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2017-03-26 23:08:10 -0300

    Implementado o contador de texto digitado

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,7 +1,20 @@
 var frase = $('.frase').text()
-var numPalavras = frase.split(' ').length
+var numPalavras = contaPalavras(frase)
 var tamanhoFrase = $('#tamanho-frase')
-
 tamanhoFrase.text(numPalavras)
 
+var campo = $('.campo-digitacao')
+campo.on('input', function () {
+  var frase = $('.campo-digitacao').val()
+  var numPalavras = contaPalavras(frase)
+  var contadorCaracteres = $('#contador-caracteres')
+  var contadorPalavras = $('#contador-palavras')
+  contadorPalavras.text(numPalavras)
+  contadorCaracteres.text(frase.length)
+})
+
+function contaPalavras (frase) {
+  var numPalavras = frase.split(/\S+/).length - 1
+  return numPalavras
+}
 
