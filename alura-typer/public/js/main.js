cf1ab91e8699933bb72f0a1349d6454fcc206cc7
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2017-03-28 22:53:15 -0300

    Incluido gameover

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,17 +1,27 @@
 var frase = $('.frase').text()
 var numPalavras = contaPalavras(frase)
 var tamanhoFrase = $('#tamanho-frase')
+var tempoRestante = $("#tempo-digitacao").text()
 tamanhoFrase.text(numPalavras)
 
 var campo = $('.campo-digitacao')
 campo.on('input', function () {
   var frase = $('.campo-digitacao').val()
   var numPalavras = contaPalavras(frase)
-  var contadorCaracteres = $('#contador-caracteres')
-  var contadorPalavras = $('#contador-palavras')
-  contadorPalavras.text(numPalavras)
-  contadorCaracteres.text(frase.length)
+  var contadorCaracteres = $('#contador-caracteres').text(frase.length)
+  var contadorPalavras = $('#contador-palavras').text(numPalavras)
 })
+
+campo.one("focus", function() {
+    var cronometroID = setInterval(function() {
+        tempoRestante--;
+        $("#tempo-digitacao").text(tempoRestante);
+        if (tempoRestante < 1) {
+            campo.attr("disabled", true);
+            clearInterval(cronometroID);
+        }
+    }, 1000);
+});
 
 function contaPalavras (frase) {
   var numPalavras = frase.split(/\S+/).length - 1
