9564ef5d57dd02c3c7c811381f9138585a4cf0ad
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2017-04-02 23:13:16 -0300

    Implementação do CSS

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,56 +1,77 @@
 /* global $ */
-// as duas vars já devem existir
+// as duas vars jï¿½ devem existir
 var campo = $('.campo-digitacao')
 var tempoInicial = $('#tempo-digitacao').text()
 $('#botao-reiniciar').click(reiniciaJogo)
 
 $(function () {
-  atualizaTamanhoFrase()
-  inicializaContadores()
-  inicializaCronometro()
-  $('#botao-reiniciar').click(reiniciaJogo)
+    atualizaTamanhoFrase()
+    inicializaContadores()
+    inicializaCronometro()
+    inicializaMarcadores()
+    $('#botao-reiniciar').click(reiniciaJogo)
 })
 
-function inicializaContadores () {
-  campo.on('input', function () {
-    var conteudo = campo.val()
+function inicializaContadores() {
+    campo.on('input', function () {
+        var conteudo = campo.val()
 
-    var qtdPalavras = conteudo.split(/\S+/).length - 1
-    $('#contador-palavras').text(qtdPalavras)
+        var qtdPalavras = conteudo.split(/\S+/).length - 1
+        $('#contador-palavras').text(qtdPalavras)
 
-    var qtdCaracteres = conteudo.length
-    $('#contador-caracteres').text(qtdCaracteres)
-  })
+        var qtdCaracteres = conteudo.length
+        $('#contador-caracteres').text(qtdCaracteres)
+    })
 }
 
-function inicializaCronometro () {
-  var tempoRestante = $('#tempo-digitacao').text()
-  $('#botao-reiniciar').attr('disabled', true)
-  campo.one('focus', function () {
-    var cronometroID = setInterval(function () {
-      tempoRestante--
-      $('#tempo-digitacao').text(tempoRestante)
-      if (tempoRestante < 1) {
-        campo.attr('disabled', true)
-        clearInterval(cronometroID)
-        $('#botao-reiniciar').attr('disabled', false)
-      }
-    }, 1000)
-  })
+function inicializaCronometro() {
+    var tempoRestante = $('#tempo-digitacao').text()
+    $('#botao-reiniciar').attr('disabled', true)
+    campo.one('focus', function () {
+        var cronometroID = setInterval(function () {
+            tempoRestante--
+            $('#tempo-digitacao').text(tempoRestante)
+            if (tempoRestante < 1) {
+                campo.attr('disabled', true)
+                clearInterval(cronometroID)
+                $('#botao-reiniciar').attr('disabled', false)
+                campo.addClass('campo-desativado')
+            }
+        }, 1000)
+    })
 }
 
-function reiniciaJogo () {
-  campo.attr('disabled', false)
-  campo.val('')
-  $('#contador-palavras').text('0')
-  $('#contador-caracteres').text('0')
-  $('#tempo-digitacao').text(tempoInicial)
-  inicializaCronometro() // novo
+function reiniciaJogo() {
+    campo.attr('disabled', false)
+    campo.val('')
+    $('#contador-palavras').text('0')
+    $('#contador-caracteres').text('0')
+    $('#tempo-digitacao').text(tempoInicial)
+    inicializaCronometro() // novo
+    campo.removeClass('campo-desativado')
+    campo.removeClass("borda-vermelha")
+    campo.removeClass("borda-verde")
 }
 
-function atualizaTamanhoFrase () {
-  var frase = $('.frase').text()
-  var numPalavras = frase.split(' ').length
-  var tamanhoFrase = $('#tamanho-frase')
-  tamanhoFrase.text(numPalavras)
+function atualizaTamanhoFrase() {
+    var frase = $('.frase').text()
+    var numPalavras = frase.split(' ').length
+    var tamanhoFrase = $('#tamanho-frase')
+    tamanhoFrase.text(numPalavras)
+}
+
+function inicializaMarcadores() {
+
+    var frase = $(".frase").text()
+    campo.on("input", function () {
+        var digitado = campo.val()
+        var comparavel = frase.substr(0, digitado.length)
+        if (digitado == comparavel) {
+            campo.addClass("borda-verde")
+            campo.removeClass("borda-vermelha")
+        } else {
+            campo.addClass("borda-vermelha")
+            campo.removeClass("borda-verde")
+        }
+    })
 }
